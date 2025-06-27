f8d2ba5d68eea87b8792bbb7083101a265e33406
Eduardo Barreira <edubarreira1212@gmail.com>
2025-06-26 23:02:36 -0300

    feat: implement user creation functionality in the controller

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -1,9 +1,33 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"socialmedia-api/src/database"
+	"socialmedia-api/src/models"
+	"socialmedia-api/src/repositories"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Creating user!"))
+	parameters, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+	if err = json.Unmarshal(parameters, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.Conect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repositories.NewUserRepository(db)
+	repository.Create(user)
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
