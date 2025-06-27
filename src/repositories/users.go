86f5b508c4732aad550624a1bb7a61563585960f
Eduardo Barreira <edubarreira1212@gmail.com>
2025-06-26 23:14:19 -0300

    feat: implement user creation functionality in the user repository

--- src/repositories/users.go
+++ src/repositories/users.go
@@ -13,6 +13,23 @@ func NewUserRepository(db *sql.DB) *User {
 	return &User{db}
 }
 
-func (u User) Create(user models.User) (uint64, error) {
-	return 0, nil
+func (repository User) Create(user models.User) (uint64, error) {
+	statement, err := repository.db.Prepare(
+		"INSERT INTO Users (name, nickName, email, password) VALUES(?, ?, ?, ?)",
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	result, err := statement.Exec(user.Name, user.NickName, user.Email, user.Password)
+	if err != nil {
+		return 0, err
+	}
+
+	lastIdInserted, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(lastIdInserted), nil
 }
