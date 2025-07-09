5fd5fd65c5ed80f27529c498cbea9e3e4e4f4925
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-09 18:18:02 -0300

    feat: implement Search method for user repository

--- src/repositories/users.go
+++ src/repositories/users.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"database/sql"
+	"fmt"
 	"socialmedia-api/src/models"
 )
 
@@ -32,4 +33,37 @@ func (repository User) Create(user models.User) (uint64, error) {
 	}
 
 	return uint64(lastIdInserted), nil
+}
+
+func (repository User) Search(nameOrNick string) ([]models.User, error) {
+	nameOrNick = fmt.Sprintf("%%%s%%", nameOrNick)
+
+	rows, err := repository.db.Query(
+		"SELECT id, name, nickName, email, createdAt FROM users WHERE name LIKE ? OR nickName LIKE ?",
+		nameOrNick, nameOrNick,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []models.User
+
+	for rows.Next() {
+		var user models.User
+
+		if err := rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.NickName,
+			&user.Email,
+			&user.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
 }
