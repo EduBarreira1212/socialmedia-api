2df13dd2506706d4d61f47cbf9b74295b898cc5b
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-09 17:46:15 -0300

    feat: implement user validation and formatting methods

--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -9,4 +13,39 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+func (user *User) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("name is mandatory")
+	}
+
+	if user.NickName == "" {
+		return errors.New("nickname is mandatory")
+	}
+
+	if user.Email == "" {
+		return errors.New("e-mail is mandatory")
+	}
+
+	if user.Password == "" {
+		return errors.New("password is mandatory")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.NickName = strings.TrimSpace(user.NickName)
+	user.Email = strings.TrimSpace(user.Email)
 }
