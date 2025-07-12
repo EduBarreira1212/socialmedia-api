3559059d13c5e64768edb2dab68cd88c7bf5a853
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-12 17:23:55 -0300

    feat: implement SearchByEmail function to retrieve user by email

--- src/repositories/users.go
+++ src/repositories/users.go
@@ -126,3 +126,21 @@ func (repository User) Delete(ID uint64) error {
 
 	return nil
 }
+
+func (repository User) SearchByEmail(email string) (models.User, error) {
+	row, err := repository.db.Query("SELECT id, password FROM users WHERE email = ?", email)
+	if err != nil {
+		return models.User{}, err
+	}
+	defer row.Close()
+
+	var user models.User
+
+	if row.Next() {
+		if err = row.Scan(&user.ID, &user.Password); err != nil {
+			return models.User{}, err
+		}
+	}
+
+	return user, nil
+}
