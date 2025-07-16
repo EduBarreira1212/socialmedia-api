012ee1128e46a7c7f1c960efe0f9c0f886539675
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-16 17:32:45 -0300

    feat: use SecretKey from config for JWT signing in CreateToken function

--- src/auth/auth.go
+++ src/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"socialmedia-api/src/config"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -12,5 +13,5 @@ func CreateToken(userID uint64) (string, error) {
 	permissions["exp"] = time.Now().Add(time.Hour * 6).Unix()
 	permissions["userId"] = userID
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
-	return token.SignedString([]byte("secret"))
+	return token.SignedString([]byte(config.SecretKey))
 }
