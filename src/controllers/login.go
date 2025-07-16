4b18374b9973964344a5b363c3ddf395c045d28b
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-16 18:03:15 -0300

    fix: handle error from CreateToken function in Login handler

--- src/controllers/login.go
+++ src/controllers/login.go
@@ -44,7 +44,11 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	token, _ := auth.CreateToken(userSaved.ID)
+	token, err := auth.CreateToken(userSaved.ID)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
 
 	w.Write([]byte(token))
 }
