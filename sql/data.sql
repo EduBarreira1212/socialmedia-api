11f4eb778043bb17675a9621ea801affe4179728
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-22 19:04:32 -0300

    feat: add initial user and follower data to the database

---
+++ sql/data.sql
@@ -0,0 +1,11 @@
+insert into users (name, nickName, email, password)
+values
+("User 1", "user_1", "user1@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- user1
+("User 2", "user_2", "user2@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- user2
+("User 3", "user_3", "user3@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"); -- user3
+
+insert into followers(user_id, follower_id)
+values
+(1, 2),
+(3, 1),
+(1, 3);
