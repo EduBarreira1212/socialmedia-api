fcae8564178f340572c890004c9accfeac321570
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-23 18:27:45 -0300

    feat: add StopFollowing function to manage unfollowing users

--- src/repositories/users.go
+++ src/repositories/users.go
@@ -160,3 +160,19 @@ func (repository User) ToFollow(userID, followerID uint64) error {
 
 	return nil
 }
+
+func (repository User) StopFollowing(userID, followerID uint64) error {
+	statement, err := repository.db.Prepare(
+		"DELETE FROM followers WHERE user_id = ? AND follower_id = ?",
+	)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+
+	if _, err := statement.Exec(userID, followerID); err != nil {
+		return err
+	}
+
+	return nil
+}
