0d0647fad6757885c33705ae2425180d59e3b11f
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-26 19:15:44 -0300

    feat: add UpdatePassword function to allow users to change their passwords

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -10,6 +10,7 @@ import (
 	"socialmedia-api/src/models"
 	"socialmedia-api/src/repositories"
 	"socialmedia-api/src/responses"
+	"socialmedia-api/src/security"
 	"strconv"
 	"strings"
 
@@ -304,4 +305,68 @@ func GetFollowing(w http.ResponseWriter, r *http.Request) {
 	}
 
 	responses.JSON(w, http.StatusOK, following)
+}
+
+func UpdatePassword(w http.ResponseWriter, r *http.Request) {
+	userIDInToken, err := auth.ExtractUserID(r)
+	if err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	params := mux.Vars(r)
+	userID, err := strconv.ParseUint(params["userID"], 10, 64)
+	if err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if userIDInToken != userID {
+		responses.Error(w, http.StatusForbidden, errors.New("isn't possible to update other user's password"))
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	var password models.Password
+	if err = json.Unmarshal(body, &password); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Conect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewUserRepository(db)
+	passwordInDB, err := repository.GetPassword(userID)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err = security.VerifyPassword(passwordInDB, password.Current); err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	passwordWithHash, err := security.Hash(password.New)
+	if err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err = repository.UpdatePassword(userID, string(passwordWithHash)); err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, nil)
 }
