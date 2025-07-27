164f4eda612b735c177c299bcc7b90d685235782
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-27 18:34:52 -0300

    feat: add Prepare, Validate, and Format methods to Post model for data handling

--- src/models/post.go
+++ src/models/post.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Post struct {
 	ID         uint64    `json:"id,omitempty"`
@@ -10,4 +14,30 @@ type Post struct {
 	AuthorNick string    `json:"authorNick,omitempty"`
 	Likes      uint64    `json:"likes"`
 	CreatedAt  time.Time `json:"createdAt,omitempty"`
+}
+
+func (post *Post) Prepare() error {
+	if err := post.Validate(); err != nil {
+		return err
+	}
+
+	post.Format()
+	return nil
+}
+
+func (post *Post) Validate() error {
+	if post.Title == "" {
+		return errors.New("title is required")
+	}
+
+	if post.Content == "" {
+		return errors.New("content is required")
+	}
+
+	return nil
+}
+
+func (post *Post) Format() {
+	post.Title = strings.TrimSpace(post.Title)
+	post.Content = strings.TrimSpace(post.Content)
 }
