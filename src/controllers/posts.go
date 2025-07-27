9de9472f1c5f48e7b6f56598909ddb10de8ec71f
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-27 17:53:34 -0300

    feat: implement initial post controller with CRUD function stubs

---
+++ src/controllers/posts.go
@@ -0,0 +1,35 @@
+package controllers
+
+import "net/http"
+
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Getposts(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeletePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetPostsByUserId(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func LikePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UnlikePost(w http.ResponseWriter, r *http.Request) {
+
+}
