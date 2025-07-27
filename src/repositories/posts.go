93a25db77fdf2fe96d81337961667ce67193c4aa
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-27 19:22:30 -0300

    feat: implement GetByID method in Posts repository for retrieving posts by ID

--- src/repositories/posts.go
+++ src/repositories/posts.go
@@ -34,3 +34,30 @@ func (repository Posts) Create(post models.Post) (uint64, error) {
 
 	return uint64(id), nil
 }
+
+func (repository Posts) GetByID(id uint64) (models.Post, error) {
+	row, err := repository.db.Query(`
+		SELECT p.*, u.Nickname FROM posts p INNER JOIN users u ON u.id = p.author_id WHERE p.id = ?`,
+		id)
+	if err != nil {
+		return models.Post{}, err
+	}
+	defer row.Close()
+
+	var post models.Post
+
+	if row.Next() {
+		if err := row.Scan(
+			&post.ID,
+			&post.Title,
+			&post.Content,
+			&post.AuthorID,
+			&post.Likes,
+			&post.CreatedAt,
+			&post.AuthorNick,
+		); err != nil {
+			return models.Post{}, err
+		}
+	}
+	return post, nil
+}
