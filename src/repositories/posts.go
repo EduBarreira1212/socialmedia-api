2a311583cfbaa2c36550d02dccd19571494bda29
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-27 18:26:36 -0300

    feat: implement post repository with Create function for adding new posts

---
+++ src/repositories/posts.go
@@ -0,0 +1,36 @@
+package repositories
+
+import (
+	"database/sql"
+	"socialmedia-api/src/models"
+)
+
+type Posts struct {
+	db *sql.DB
+}
+
+func NewPostRepository(db *sql.DB) *Posts {
+	return &Posts{db}
+}
+
+func (repository Posts) Create(post models.Post) (uint64, error) {
+	statement, err := repository.db.Prepare(
+		"INSERT INTO posts (title, content, author_id) VALUES (?, ?, ?)",
+	)
+	if err != nil {
+		return 0, err
+	}
+	defer statement.Close()
+
+	result, err := statement.Exec(post.Title, post.Content, post.AuthorID)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(id), nil
+}
