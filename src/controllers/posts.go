f1d472086cfb6d34d3759f3c3961267750c1c1e7
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-27 18:26:42 -0300

    feat: implement CreatePost function with user authentication and error handling

--- src/controllers/posts.go
+++ src/controllers/posts.go
@@ -1,9 +1,52 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"socialmedia-api/src/auth"
+	"socialmedia-api/src/database"
+	"socialmedia-api/src/models"
+	"socialmedia-api/src/repositories"
+	"socialmedia-api/src/responses"
+)
 
 func CreatePost(w http.ResponseWriter, r *http.Request) {
+	userID, err := auth.ExtractUserID(r)
+	if err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
 
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var post models.Post
+	if err = json.Unmarshal(body, &post); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	post.AuthorID = userID
+
+	db, err := database.Conect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewPostRepository(db)
+	post.ID, err = repository.Create(post)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusCreated, post)
 }
 
 func Getposts(w http.ResponseWriter, r *http.Request) {
