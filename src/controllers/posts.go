830f79135ad4f36780585250ba4f54a7447a8f4b
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-28 18:11:26 -0300

    feat: implement DeletePost controller

--- src/controllers/posts.go
+++ src/controllers/posts.go
@@ -166,7 +166,44 @@ func UpdatePost(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeletePost(w http.ResponseWriter, r *http.Request) {
+	userID, err := auth.ExtractUserID(r)
+	if err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
 
+	params := mux.Vars(r)
+	postID, err := strconv.ParseUint(params["postID"], 10, 64)
+	if err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Conect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewPostRepository(db)
+	postInDB, err := repository.GetByID(postID)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if postInDB.AuthorID != userID {
+		responses.Error(w, http.StatusForbidden, errors.New("you cannot delete a post that is not yours"))
+		return
+	}
+
+	if err = repository.Delete(postID); err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, nil)
 }
 
 func GetPostsByUserId(w http.ResponseWriter, r *http.Request) {
